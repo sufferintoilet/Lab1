75a1491d1a091ac0806608de0e66c83128f445a8
sufferintoilet <141156910+sufferintoilet@users.noreply.github.com>
2023-12-06 22:53:46 +0900

    Create README.md

---
+++ SystemTime/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+namespace Systemtime
+{
+class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime localTime = DateTime.Now;
+        Console.WriteLine("Thoi gian hien tai: " + localTime);
+    }
+}
+}
